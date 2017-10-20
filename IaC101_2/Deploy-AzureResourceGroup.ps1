079c61bb398c04a6299190366612c861cd351121
Ajeet Kumar Chouksey <ajeet.kumar.chouksey@avanade.com>
2017-10-20 15:57:07 +0530

    Inital code with copyIndex and Public IP

--- IaC101_2/Deploy-AzureResourceGroup.ps1
+++ IaC101_2/Deploy-AzureResourceGroup.ps1
@@ -1,8 +1,8 @@
 #Requires -Version 3.0
 
 Param(
-    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'AzureResourceGroup1',
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,  
+   [string] $ResourceGroupName="IaCLab",
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
